abf597090a9e9a540b9d6a161d2eedd6f9cf7d96
leoafarias <leoafarias@gmail.com>
2022-09-22 08:24:26 -0400

    Update dependencies and scaffolding

---
+++ android/app/src/main/kotlin/com/example/remix_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.remix_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/leofarias/flutter"
+export "FLUTTER_ROOT=/Users/leofarias/fvm/versions/stable"
 export "FLUTTER_APPLICATION_PATH=/Users/leofarias/Concepta/remix_ui"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
