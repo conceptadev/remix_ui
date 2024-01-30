a649f88ea21853c70afe0430cc73f031dd73f95e
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2024-01-30 17:26:04 -0300

    Implement widgetbook

---
+++ demo/lib/components/checkbox.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:remix_ui/remix_ui.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+@widgetbook.UseCase(
+  name: 'with title and icon',
+  type: RemixCheckbox, // Changed from RemixButton to RemixCheckbox
+)
+Widget buildCheckboxUseCase(BuildContext context) {
+  return Center(
+    child: RemixCheckbox(
+      label: context.knobs.stringOrNull(
+        label: 'Title',
+        initialValue: 'Title',
+      ),
+      onChanged: (value) {},
+      isChecked: context.knobs.boolean(
+        label: 'Checked',
+        initialValue: false,
+      ),
+      isDisabled: context.knobs.boolean(
+        label: 'Disabled',
+        initialValue: false,
+      ),
+    ),
+  );
+}
--- lib/components/button/button.variants.dart
+++ lib/components/button/button.variants.dart
@@ -3,19 +3,35 @@ import 'package:mix/mix.dart';
 class ButtonSize extends Variant {
   const ButtonSize._(super.name);
   // Sizes
-  static const xsmall = ButtonSize._('xsmallButtonVariant');
-  static const small = ButtonSize._('smallButtonVariant');
-  static const medium = ButtonSize._('mediumButtonVariant');
-  static const large = ButtonSize._('largeButtonVariant');
+  static const xsmall = ButtonSize._('remix.button.xsmall');
+  static const small = ButtonSize._('remix.button.small');
+  static const medium = ButtonSize._('remix.button.medium');
+  static const large = ButtonSize._('remix.button.large');
+
+  static const values = [
+    xsmall,
+    small,
+    medium,
+    large,
+  ];
 }
 
 class ButtonType extends Variant {
   const ButtonType._(super.name);
   // Types
-  static const primary = ButtonType._('primaryButtonVariant');
-  static const secondary = ButtonType._('secondaryButtonVariant');
-  static const destructive = ButtonType._('destructiveButtonVariant');
-  static const outline = ButtonType._('outlineButtonVariant');
-  static const ghost = ButtonType._('ghostButtonVariant');
-  static const link = ButtonType._('linkButtonVariant');
+  static const primary = ButtonType._('remix.button.primary');
+  static const secondary = ButtonType._('remix.button.secondary');
+  static const destructive = ButtonType._('remix.button.destructive');
+  static const outline = ButtonType._('remix.button.outline');
+  static const ghost = ButtonType._('remix.button.ghost');
+  static const link = ButtonType._('remix.button.link');
+
+  static const values = [
+    primary,
+    secondary,
+    destructive,
+    outline,
+    ghost,
+    link,
+  ];
 }
